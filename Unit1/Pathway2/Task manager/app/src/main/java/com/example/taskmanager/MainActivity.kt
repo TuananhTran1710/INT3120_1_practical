c20fdde99b1b501e9bcf4090c490feb29c64adc3
TranTuanAnh <vt34023@gmail.com>
2025-09-13 01:03:43 +0700

    Unit1-path2-Task manager

---
+++ Unit1/Pathway2/Task manager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskManagerTheme {
+                Surface(modifier= Modifier.fillMaxSize(),
+                    color= MaterialTheme.colorScheme.background
+                ){
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(modifier = Modifier.fillMaxSize(),verticalArrangement = Arrangement.Center, )
+    {
+        Image(painter = image,
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.CenterHorizontally))
+        Text(text = stringResource(R.string.firstText),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp).fillMaxWidth(),
+            textAlign = TextAlign.Center)
+        Text(
+                text = stringResource(R.string.secondText),
+                fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+            )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        Greeting()
+    }
+}
